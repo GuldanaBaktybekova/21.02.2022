8f43336c26738e3a25f00e8967d4274dfebed75d
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-21 15:09:36 +0600

    added classlist to js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.querySelector('select').addEventListener('input', function(){
   const value = this.value
   document.querySelectorAll('ul').forEach(function(ul){
+    // ul.className = value === ul.id ? '' : 'hidden'
+    // ul.classList.toggle('hidden', this.value !== ul.id)
    if(value === ul.id) {
     ul.className = '';
    }
@@ -10,3 +12,4 @@ document.querySelector('select').addEventListener('input', function(){
 
   });
 });
+
