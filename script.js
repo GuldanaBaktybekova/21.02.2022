8b8da1c993b69de681fe64cb9c97ca6b2ecc0dbd
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-24 18:59:12 +0600

    Added HTML to index2.html

--- script.js
+++ script.js
@@ -56,3 +56,61 @@ articles.forEach(article => {
   main.append
  
 })
+
+pts.js
+// document.querySelector('select').addEventListener('input', function() {
+//   document.querySelectorAll('ul').forEach(
+//     ul => ul.classList.toggle('hidden', this.value !== ul.id)
+//   );
+// });
+
+// document.body.classList.toggle('hello');
+
+// document.body.classList.add('hello');
+// document.body.classList.remove('hello');
+
+// if (document.body.classList.contains('bye')) {
+
+// }
+
+// document.body.classList.replace('hello', 'bye');
+
+
+const articles = [
+  {
+    id: "article-1",
+    title: "Hello world",
+    image: "./photo.jpeg",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+  },
+  {
+    id: "article-2",
+    title: "Hello world",
+    image: "./photo.jpeg",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+  },
+  {
+    id: "article-3",
+    title: "Hello world",
+    image: "./photo.jpeg",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+  }
+];
+
+const main = document.querySelector('main');
+articles.forEach(article => {
+  const articleElement = document.createElement('article');
+  const h1 = document.createElement('h1');
+  const img = document.createElement('img');
+  const p = document.createElement('p');
+
+  articleElement.id = article.id;
+  h1.textContent = article.title;
+  img.src = article.image;
+  p.textContent = article.body;
+
+  articleElement.append(h1);
+  articleElement.append(img);
+  articleElement.append(p);
+  main.append(articleElement);
+})
