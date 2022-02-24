c87a95b319c74f98b881261d2d33288dc9062ef0
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-24 20:04:50 +0600

    Added if to js

--- script.js
+++ script.js
@@ -22,42 +22,8 @@
 
 // document.body.classList.contains('hello');
 
-const articles = [
-  {
-    id: "article-1",
-    title: "Hello world",
-    image: "https://cdn.britannica.com/26/162626-050-3534626F/Koala.jpg?w=400&h=225&c=crop",
-    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel excepturi eveniet doloremque iure velit earum! Enim, voluptatibus culpa quasi ex ut blanditiis ea velit, fuga praesentium nesciunt ad eum! Eveniet!"
-  },
-  {
-    id: "article-2",
-    title: "Hello world",
-    image: "https://cdn.britannica.com/26/162626-050-3534626F/Koala.jpg?w=400&h=225&c=crop",
-    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel excepturi eveniet doloremque iure velit earum! Enim, voluptatibus culpa quasi ex ut blanditiis ea velit, fuga praesentium nesciunt ad eum! Eveniet!"
-  },
-  {
-    id: "article-3",
-    title: "Hello world",
-    image: "https://cdn.britannica.com/26/162626-050-3534626F/Koala.jpg?w=400&h=225&c=crop",
-    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel excepturi eveniet doloremque iure velit earum! Enim, voluptatibus culpa quasi ex ut blanditiis ea velit, fuga praesentium nesciunt ad eum! Eveniet!"
-  }
-]
 
-const main = document.querySelector('main');
-articles.forEach(article => {
-  const article = document.createElement('article');
-  const h1 = document.createElement('h1');
-  const img = document.createElement('img');
-  const p = document.createElement('p');
 
-  articleElement.append(h1);
-  articleElement.append()
-  articleElement.append()
-  main.append
- 
-})
-
-pts.js
 // document.querySelector('select').addEventListener('input', function() {
 //   document.querySelectorAll('ul').forEach(
 //     ul => ul.classList.toggle('hidden', this.value !== ul.id)
@@ -80,19 +46,19 @@ const articles = [
   {
     id: "article-1",
     title: "Hello world",
-    image: "./photo.jpeg",
+    image: "https://oir.mobi/uploads/posts/2021-05/1620871320_13-oir_mobi-p-lenivets-i-koala-zhivotnie-krasivo-foto-15.jpg",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
   },
   {
     id: "article-2",
     title: "Hello world",
-    image: "./photo.jpeg",
+    image: "https://oir.mobi/uploads/posts/2021-05/1620871320_13-oir_mobi-p-lenivets-i-koala-zhivotnie-krasivo-foto-15.jpg",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
   },
   {
     id: "article-3",
     title: "Hello world",
-    image: "./photo.jpeg",
+    image: "https://oir.mobi/uploads/posts/2021-05/1620871320_13-oir_mobi-p-lenivets-i-koala-zhivotnie-krasivo-foto-15.jpg",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
   }
 ];
@@ -114,3 +80,25 @@ articles.forEach(article => {
   articleElement.append(p);
   main.append(articleElement);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('h1').addEventListener('click', function(){
+
+  document.querySelectorAll('article').forEach(function(artilcle){
+    // article.id = className === article.id ? '' : 'hidden'
+    
+
+
+  });
+});
