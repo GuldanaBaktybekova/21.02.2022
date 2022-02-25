e883aeb3381546b8119bd2408bd9c2e44bc77cb2
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-25 15:51:34 +0600

    Added foreach to js

--- script.js
+++ script.js
@@ -84,34 +84,10 @@
 
 
 
-
-
-
-
-
-let article1 = document.getElementById('article1');
-article1.style.display = "none";
-document.getElementById('art1').addEventListener('click',function(){
-  article1.style.display = "block";
-});
-document.getElementById('art1').addEventListener('dblclick',function(){
-  article1.style.display = "none";
-});
-
-let article2 = document.getElementById('article2');
-article2.style.display = "none";
-document.getElementById('art2').addEventListener('click',function(){
-  article2.style.display = "block";
-});
-document.getElementById('art2').addEventListener('dblclick',function(){
-  article2.style.display = "none";
-});
-
-let article3 = document.getElementById('article3');
-article3.style.display = "none";
-document.getElementById('art3').addEventListener('click',function(){
-  article3.style.display = "block";
-});
-document.getElementById('art3').addEventListener('dblclick',function(){
-  article3.style.display = "none";
+let details = document.querySelectorAll('details');
+details.forEach((detail) => {
+  detail.addEventListener('toggle', function(e) {
+    let article = document.querySelector(`${e.target.id}`);
+    article.style.display = 'none';
+  });
 });
